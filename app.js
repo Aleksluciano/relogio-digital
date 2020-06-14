3818a8672e060cdc67ab103da347199e7adf00b2
Alex Avelar <aleksluciano@gmail.com>
2020-06-14 01:00:12 -0300

    pwa add

--- app.js
+++ app.js
@@ -19,3 +19,15 @@ relogio.innerHTML = html;
 
 setInterval(ticktack, 1000);
 
+
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err));
+    });
+  }
+
---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticRelogio = "relogio-site";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticRelogio).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
